611b6e538db319741b2c02a78e500f10e7073714
anicolette <anicolette@email.arizona.edu>
2015-12-05 12:53:14 -0700

    Added query to insert a new employee

--- DatabaseController.java
+++ DatabaseController.java
@@ -38,9 +38,9 @@ public class DatabaseController {
 
   public DatabaseController() {
     // your cs login name
-    username = "anicolette"; 
+    username = "username"; 
     // your Oracle password, NNNN is the last four digits of your CSID
-    password = "a0374";
+    password = "password";
     connect_string_ = "jdbc:oracle:thin:@aloe.cs.arizona.edu:1521:oracle";
   }
 
