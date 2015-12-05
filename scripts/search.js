dbdeef47b1a86fcc3787128797eaae1bc83cb962
cjkarl <cjkarl@email.arizona.edu>
2015-12-05 14:58:03 -0700

    Added Forms

--- scripts/search.js
+++ scripts/search.js
@@ -1,6 +1,45 @@
 function Search() {
   window.location = "searchprocess.jsp";
 }
-function addFormSearch() {
-	window.location = "addform.html";
+function insertionIndexSearch() {
+	window.location = "insertionIndex.html"
+}
+function carAddFormSearch() {
+	window.location = "forms/carAddForm.html";
+}
+function clientAddFormSearch() {
+	window.location = "forms/clientAddForm.html";
+}
+function drivingTestAddFormSearch() {
+	window.location = "forms/drivingTestAddForm.html";
+}
+function employeeAddFormSearch() {
+	window.location = "forms/employeeAddForm.html";
+}
+function failureAddFormSearch() {
+	window.location = "forms/failureAddForm.html";
+}
+function faultAddFormSearch() {
+	window.location = "forms/faultAddForm.html";
+}
+function inspectionAddFormSearch() {
+	window.location = "forms/inspectionAddForm.html";
+}
+function interviewAddFormSearch() {
+	window.location = "forms/interviewAddForm.html";
+}
+function lessonAddFormSearch() {
+	window.location = "forms/lessonAddForm.html";
+}
+function needAddFormSearch() {
+	window.location = "forms/needAddForm.html";
+}
+function noteAddFormSearch() {
+	window.location = "forms/noteAddForm.html";
+}
+function officeAddFormSearch() {
+	window.location = "forms/officeAddForm.html";
+}
+function roleAddFormSearch() {
+	window.location = "forms/roleAddForm.html";
 }
